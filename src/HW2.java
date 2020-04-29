1f544bf6142cb0e3154458804401e901ca13d087
yjoon <songjun5711@gmail.com>
2020-04-29 18:22:20 +0900

    gg

--- src/HW2.java
+++ src/HW2.java
@@ -1,11 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Random;
 import java.util.Scanner;
 
 public class HW2 {
-
     public static void main(String[] args) {
         Tree234<String, Integer> st = new Tree234<>();
         Scanner sc = new Scanner(System.in);
@@ -20,9 +20,9 @@ public class HW2 {
             long start = System.currentTimeMillis();
             while (sc.hasNext()) {
                 String word = sc.next();
-                if (!st.contains(word))
+                if (!st.contains(word)) {
                     st.put(word, 1);
-                else st.put(word, st.get(word) + 1);
+                } else st.put(word, st.get(word) + 1);
             }
             long end = System.currentTimeMillis();
             System.out.println("입력 완료: 소요 시간 = " + (end - start) + "ms");
@@ -35,11 +35,11 @@ public class HW2 {
             for (int i = 0; i < loopCount; i++) {
                 int deletedIndex = rand.nextInt(keyList.size());
                 String key = keyList.get(deletedIndex);
-                st.delete(key);
+                //st.delete(key);
                 keyList.remove(deletedIndex);
             }
-            System.out.println("\n### 키 삭제 후 트리 정보");
-            print_tree(st);
+            //System.out.println("\n### 키 삭제 후 트리 정보");
+            // print_tree(st);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -67,7 +67,9 @@ class Tree234<String, Integer> {
     private int nodeSize = 0;
 
     public void put(String key, Integer value) {
+
         Node<String, Integer> x = root;
+
         if (x == null) {
             root = new Node<>(key, value);
             nodeSize++;
@@ -88,12 +90,33 @@ class Tree234<String, Integer> {
         node.data.sort((o1, o2) -> o1.compareTo(o2.key));
         if (node.data.size() == 4) {
             if (node.parent == null) {
-                node.parent = new Node<>(node.data.get(2).key, node.data.get(2).value);
+                node.parent = new Node<>(node.data.get(2).key, node.data.remove(2).value);
                 node.parent.child.add(node);
-                node.parent.child.add(new Node<>(node.data.get(2).key, node.data.get(2).value));
+                node.parent.child.add(new Node<>(node.data.get(2).key, node.data.remove(2).value));
+                node.parent.child.get(1).parent = node.parent;
+                root = node.parent;
             } else {
-                insert(node.parent, node.data.remove(2).key, node.data.remove(2).value);
-                //TODO parent가 있을때 insert 그리고 child 비교해서 sort하고 다시 배당하기
+                insert(node.parent, node.data.get(2).key, node.data.remove(2).value);
+                node.parent.child.add(new Node<>(node.data.get(2).key, node.data.remove(2).value));
+                node.parent.child.get(node.parent.child.size() - 1).parent = node.parent;
+                node.parent.child.sort(new Comparator<Node<String, Integer>>() {
+                    @Override
+                    public int compare(Node<String, Integer> o1, Node<String, Integer> o2) {
+                        return o1.data.get(0).compareTo(o2.data.get(0).key);
+                    }
+                });
+                if (node.parent.child.size() == 5) {
+                    int pos = 0;
+                    for (int i = 0; i < node.parent.parent.child.size(); i++) {
+                        if (node.parent.parent.child.get(i).equals(node.parent))
+                            pos = i;
+                    }
+                    node.parent.parent.child.get(pos).child.add(node.parent.child.remove(3));
+                    node.parent.parent.child.get(pos).child.add(node.parent.child.remove(3));
+                    node.parent = node.parent.parent.child.get(pos);
+                    node.parent.child.get(1).parent = node.parent;
+                }
+
             }
         }
     }
@@ -101,17 +124,19 @@ class Tree234<String, Integer> {
 
     private Element<String, Integer> dataSearch(String key) {
         Node<String, Integer> x = root;
+        if (root == null)
+            return null;
         int pos;
         while (true) {
             pos = getDataPos(x, key);
             if (x.data.get(pos).compareTo(key) == 0)
                 return x.data.get(pos);
-            else if (x.data.get(pos).compareTo(key) < 0) {
-                if (x.child.get(pos) == null)
+            else if (x.data.get(pos).compareTo(key) > 0) {
+                if (x.child.size() == 0)
                     return null;
                 x = x.child.get(pos);
             } else {
-                if (x.child.get(pos + 1) == null)
+                if (x.child.size() == 0)
                     return null;
                 x = x.child.get(pos + 1);
             }
@@ -125,12 +150,12 @@ class Tree234<String, Integer> {
             pos = getDataPos(x, key);
             if (x.data.get(pos).compareTo(key) == 0)
                 return x;
-            else if (x.data.get(pos).compareTo(key) < 0) {
-                if (x.child == null)
+            else if (x.data.get(pos).compareTo(key) > 0) {
+                if (x.child.size() == 0)
                     return x;
                 x = x.child.get(pos);
             } else {
-                if (x.child == null)
+                if (x.child.size() == 0)
                     return x;
                 x = x.child.get(pos + 1);
             }
@@ -144,7 +169,7 @@ class Tree234<String, Integer> {
             if (node.data.get(pos).compareTo(key) >= 0)
                 return pos;
         }
-        return pos;
+        return pos -1;
     }
 
     public Integer get(String key) {
@@ -163,6 +188,23 @@ class Tree234<String, Integer> {
     }
 
     public Iterable<String> keys() {
+        ArrayList<String> keyList = new ArrayList<String>(size());
+        inorder(root, keyList);
+        return keyList;
+    }
+
+    private void inorder(Node<String, Integer> x, ArrayList<String> keyList) {
+        int dataSize = x.data.size();
+        if (x.child.size() == 0) {
+            for (int i = 0; i < dataSize; i++)
+                keyList.add(x.data.get(i).key);
+        } else {
+            for (int i = 0; i < dataSize; i++) {
+                inorder(x.child.get(i), keyList);
+                keyList.add(x.data.get(i).key);
+            }
+            inorder(x.child.get(dataSize), keyList);
+        }
     }
 
     public int size() {
@@ -172,16 +214,17 @@ class Tree234<String, Integer> {
     public int depth() {
         int depth = 0;
         Node<String, Integer> x = root;
-        while (x != null) {
+        while (true) {
             depth++;
-            x = x.child.get(0);
+            if (x.child.size() == 0)
+                return depth;
+            else
+                x = x.child.get(0);
         }
-        depth--;
-        return depth;
     }
 
-    public void delete(String key) {
-    }
+//    public void delete(String key) {
+//    }
 
 
 }
@@ -189,14 +232,15 @@ class Tree234<String, Integer> {
 class Node<K, V> {
     ArrayList<Element<K, V>> data = new ArrayList<>(4);
     Node<K, V> parent;
-    ArrayList<Node<K, V>> child = new ArrayList<>(4);
+    ArrayList<Node<K, V>> child = new ArrayList<>(5);
 
     Node(K key, V value) {
         data.add(new Element<>(key, value));
+        parent = null;
     }
 }
 
-class Element<K, V> {
+class Element<K, V> implements Comparable<K> {
     K key;
     V value;
 
@@ -205,6 +249,7 @@ class Element<K, V> {
         this.value = value;
     }
 
+    @Override
     public int compareTo(K key) {
         return this.key.toString().compareTo(key.toString());
     }
